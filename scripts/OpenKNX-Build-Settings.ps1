a395df7b2d59c2e49dc403813b833f107da06f6c
Michael Geramb <lasso_riefen0m@icloud.com>
2024-10-14 20:28:54 +0200

    Reg1-ESP support

--- scripts/OpenKNX-Build-Settings.ps1
+++ scripts/OpenKNX-Build-Settings.ps1
@@ -1,7 +1,7 @@
 # set product names, allows mapping of (devel) name in Project to a more consistent name in release
-$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "Dummy" "Dummy"
+$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "InternetServices" "InternetServices"
 
-$settings.sourceName = "Dummy"  
-$settings.targetName = "Dummy"
+$settings.sourceName = "InternetServices"  
+$settings.targetName = "InternetServices"
 
 Return $settings
