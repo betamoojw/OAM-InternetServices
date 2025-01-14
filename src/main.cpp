72c2258b3c09db9b833eccc69be901f9d778499b
Michael Geramb <lasso_riefen0m@icloud.com>
2025-01-14 20:18:49 +0100

    Add function blocks with blinker and selection

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,6 @@ AutoConnectConfig config;
 
 void setup()
 {
-#ifdef OPENKNX_CUSTOM_MASK_VERSION
-    knx.bau().deviceObject().maskVersion(OPENKNX_CUSTOM_MASK_VERSION);
-#endif
     const uint8_t firmwareRevision = 1;
     openknx.init(firmwareRevision);
 #ifdef WLAN_WifiSSID    
